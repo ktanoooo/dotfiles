f62f878d64d2272e1db9eb7967749bb2e65f2059
ktanoooo <ktanoooo1112@gmail.com>
2021-11-16 01:26:43 +0900

    Refactor git aliases

--- dotfiles/.aliases/.git.zsh
+++ dotfiles/.aliases/.git.zsh
@@ -1,6 +1,7 @@
 alias g='git'
 alias gac='git add -A :/ && git commit'
 alias gaca='git add -A :/ && git commit --amend'
+alias gwip='git add -A :/ && git commit -m "[WIP]"'
 alias gb='git branch'
 alias gca='git commit --amend'
 alias gci='git commit'
@@ -9,25 +10,25 @@ alias gdf='git diff'
 alias gdfc='git diff --cached'
 alias gfop='git fetch origin --prune'
 alias gfup='git fetch upstream --prune'
-alias gic='git init && git add -A :/ && git commit'
-alias glg="git log --stat --pretty=format:'%Cblue%h %Cgreen%ai %Cred%an %d
-%Creset%s'"
 alias glggo='git log --graph --oneline'
-alias gwip='git add -A :/ && git commit -m "[WIP]"'
 alias gp='git push'
 alias gpo='git push origin'
 alias gpl='git pull'
 alias grm='git remote -v'
 alias grma='git remote add'
 alias grms='git remote set-url origin'
-alias grbh1id='git rebase HEAD^ --ignore-date'
-alias grfh='git reset FETCH_HEAD --hard'
-alias grfhh=grfh
-alias grhh='git reset HEAD --hard'
-alias grh1h='git reset HEAD^ --hard'
+alias grh='git reset --hard origin/main'
+alias grhh='git reset --hard HEAD'
+alias grh1h='git reset --hard HEAD^'
 alias gs='git status -sb'
 alias gst='git status'
 alias gconf='git config --local --list'
+
+# gh 
+alias ghweb='gh repo view --web'
+
+# ghq
+alias cdgh='cd `ghq list -p | fzf`'
 
 gu() {
   guserinfo(){
@@ -49,9 +50,6 @@ gu() {
     echo "no change"
   fi
 }
-
-alias cdgh='cd `ghq list -p | fzf`'
-alias ghweb='gh repo view --web'
 gcre() {
   [ -z "$(ls -A ./)" ] && echo "Fail: Directory is empty." && return 0;
   git init;
@@ -70,7 +68,6 @@ gacm() {
 gcim() {
   git commit -m "$(echo $@)"
 }
-
 gpom() {
   if git branch | grep '[ \*] master$' > /dev/null; then
     git push origin master $@
