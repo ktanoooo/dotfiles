35efcb130da1da90d6895dbd789ac45f0c0874a8
ktanoooo <ktanoooo1112@gmail.com>
2022-01-22 08:54:10 +0900

    Update du command

--- dotfiles/.aliases/infra.zsh
+++ dotfiles/.aliases/infra.zsh
@@ -7,7 +7,14 @@ alias loikl='lsof -i | fzf | awk "{print \$2 }" | xargs kill -9'
 
 alias fd='fd -iH --no-ignore-vcs -E ".git" -E "node_modules"' rmds='fd .DS_Store -X rm'
 
-alias df='df -Th'
+alias dfh='df -Th'
+duh() {
+  if [ -z "$1" ]; then
+    du -h -d 1 | sort -h -r
+  else
+    du -h -d $1 | sort -h -r
+  fi
+}
 alias makedummy='dd if=/dev/zero of=/home/user/100MB.file bs=1M count=100'
 
 alias skg='ssh-keygen -t rsa -f $1'
