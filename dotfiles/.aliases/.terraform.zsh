b7918620d06783327e735ec443b8969f214f0d3a
ktanoooo <ktanoooo1112@gmail.com>
2022-02-20 03:52:13 +0900

    Add alias for terraform state

--- dotfiles/.aliases/.terraform.zsh
+++ dotfiles/.aliases/.terraform.zsh
@@ -3,3 +3,7 @@ tfpln='terraform plan'
 tfapy='terraform applay'
 tfdl='terraform destroy'
 tffmt='terraform fmt -recursive'
+
+tfsl='terraform state list'
+tfsrm='terraform state rm $1'
+tfimp='terraform import $1'
