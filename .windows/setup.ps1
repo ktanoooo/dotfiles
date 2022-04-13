04469a87276dd64017da929fad3167ad0e71b731
ktanoooo <ktanoooo1112@gmail.com>
2022-04-13 23:47:24 +0900

    Update `setup.ps1`

--- .windows/setup.ps1
+++ .windows/setup.ps1
@@ -7,26 +7,33 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Set WSL2 as default
-wsl --install
 wsl --set-default-version 2
 
-$chocoBundle = Get-Item *.txt
-
-# Install Windows Terminal
-choco install slack -y
-choco install vscode -y
-choco install discord -y
-choco install keepass -y
-choco install postman -y
-choco install powertoys -y
-choco install googlechrome -y
-choco install cascadiacode -y  # Code Font
-choco install mysql.workbench -y
-choco install microsoft-windows-terminal -y
-
+# Windows terminal setting
 # Copy settings json to C:\Users\ktanoooo\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\
 curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.windows/windows_terminal_settings.json -o $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json
 
-curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.vscode/settings.json -o $env:APPDATA\Code\User\settings.json
+# Install Powertoys
+choco install powertoys -y
+curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.windows/keyboard_manager/powertoys_default.json -o $env:USERPROFILE\AppData\Local\Microsoft\PowerToys\'Keyboard Manager'\default.json
 
+# Install VSCode
+choco install vscode -y
+curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.vscode/settings.json -o $env:APPDATA\Code\User\settings.json
 curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.vscode/keybindings.json -o $env:APPDATA\Code\User\keybindings.json
+
+# Install Browser
+choco install googlechrome -y
+
+# Install Communication Apps
+choco install slack -y
+choco install discord -y
+
+# Install Fonts
+choco install cascadiacode -y  # Code Font
+
+# Install Work tools
+choco install keepass -y
+choco install postman -y
+choco install mysql.workbench -y
+choco install microsoft-windows-terminal -y
