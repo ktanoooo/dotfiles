d5a842640542240473c8733730220b883149766f
ktanoooo <ktanoooo1112@gmail.com>
2022-09-18 13:01:40 +0900

    Add setup for deno

--- setup.sh
+++ setup.sh
@@ -62,6 +62,7 @@ install_bundle() {
 ## ----------------------------------------
 install_asdf_global() {
   asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git || true
+  asdf plugin add deno https://github.com/asdf-community/asdf-deno.git || true
   asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
   asdf plugin add golang https://github.com/kennyp/asdf-golang.git || true
   asdf plugin add python https://github.com/danhper/asdf-python.git || true
