969c4279b141074511aaed5e05dc386f02275e1d
ktanoooo <ktanoooo1112@gmail.com>
2022-10-18 19:24:19 +0900

    Update gpgdl

--- dotfiles/.aliases/.gpg.zsh
+++ dotfiles/.aliases/.gpg.zsh
@@ -22,9 +22,7 @@ alias gpgexp='gpgpub | xargs gpg --armor --export'
 ## ----------------------------------------
 gpgdl() {
   # Delete secret key
-  gpgkey | sed -n -e1p -e4p | cut -d " " -f4 | cut -d "/" -f2 | sed -n 2p | xargs gpg --delete-secret-keys
-  # Delete public key
-  gpg --delete-key ktanoooo1112@gmail.com
+  gpgpub | xargs -I{} sh -c 'gpg --delete-secret-keys {} && gpg --delete-key {}'
 }
 
 ## ----------------------------------------
