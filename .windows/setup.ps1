4ea288c5e1e96499e8cc5b0976446a6629d66c29
ktanoooo <ktanoooo1112@gmail.com>
2023-06-28 22:00:53 +0900

    Remove choco install

--- .windows/setup.ps1
+++ .windows/setup.ps1
@@ -13,34 +13,10 @@ wsl --set-default-version 2
 curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.windows/windows_terminal_settings.json -o $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 
 # Install Keyboard Manager
-choco install powertoys -y
 # curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.windows/keyboard_manager/powertoys_default.json -o $env:USERPROFILE\AppData\Local\Microsoft\PowerToys\'Keyboard Manager'\default.json
 . ./keyboard_manager/scancode_map.ps1
 curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.windows/keyboard_manager/move_cursor_like_ecmas.ahk -o $env:USERPROFILE\AppData\Roaming\Microsoft\Windows\'Start Menu'\Programs\Startup\move_cursor_like_ecmas.ahk
 
 # Install VSCode
-choco install vscode -y
 curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.vscode/settings.json -o $env:AppData\Code\User\settings.json
 curl https://raw.githubusercontent.com/ktanoooo/dotfiles/main/.vscode/keybindings.json -o $env:APPDATA\Code\User\keybindings.json
-
-# Install Browser
-choco install googlechrome -y
-
-# Install Communication Apps
-choco install slack -y
-choco install discord -y
-choco install zoom -y
-
-# Install Fonts
-choco install cascadiacode -y
-choco install nerdfont-hack -y
-
-# Install Work tools
-choco install keepassxc -y
-choco install postman -y
-choco install bloomrpc -y
-choco install mysql.workbench -y
-choco install microsoft-windows-terminal -y
-choco install dropbox -y
-choco install docker-desktop -y
-choco install openvpn -y
