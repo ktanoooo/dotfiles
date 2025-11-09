555e9c6a695a58b6681c363f87cca11bf4d66da0
ktanoooo <ktanoooo1112@gmail.com>
2025-11-09 22:40:17 +0900

    非推奨コマンドの削除

--- bundle/install.sh
+++ bundle/install.sh
@@ -8,7 +8,6 @@ EXEPATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 ## ----------------------------------------
 install_brew() {
   brew upgrade
-  brew tap "homebrew/bundle"
   brew bundle --file "${EXEPATH}/Brewfile.common"
   if [[ $OSTYPE == "linux-gnu" ]]; then
     brew bundle --file "${EXEPATH}/Brewfile.win"
