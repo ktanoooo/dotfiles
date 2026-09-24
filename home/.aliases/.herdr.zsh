8221dc71712a6bc1dc1f13682d97ca0726da719a
ktanoooo <ktanoooo1112@gmail.com>
2026-09-24 10:39:51 +0900

    Simplify quoting in hdd alias

--- home/.aliases/.herdr.zsh
+++ home/.aliases/.herdr.zsh
@@ -1,2 +1,2 @@
 alias hd='herdr'
-alias hdd='herdr session list | fzf --header-lines=1 | awk '"'"'{print $1}'"'"' | xargs -I {} herdr session stop {}'
+alias hdd="herdr session list | fzf --header-lines=1 | awk '{print \$1}' | xargs -I {} herdr session stop {}"
